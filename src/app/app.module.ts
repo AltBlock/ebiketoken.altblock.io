10bf5b91ad7bfbf657985027e16a51a0ccc30eb7
Sudhir Shrestha <shrestha201@gmail.com>
2018-04-11 10:19:49 +1000

    Import Drizzle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Drizzle, generateStore } from 'drizzle';
+
 import {
   MatButtonModule,
   MatCheckboxModule,
