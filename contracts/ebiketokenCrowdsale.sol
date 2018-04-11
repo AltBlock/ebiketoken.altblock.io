ae7c2cbc64d06fe9063e997e3649172a7477addd
Sudhir Shrestha <shrestha201@gmail.com>
2018-04-11 10:12:46 +1000

    Crowsale contract

---
+++ contracts/ebiketokenCrowdsale.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.18;
+
+import './ebiketoken.sol';
+import 'zeppelin-solidity/contracts/crowdsale/emission/MintedCrowdsale.sol';
+import 'zeppelin-solidity/contracts/crowdsale/validation/TimedCrowdsale.sol';
+
+
+contract ebiketokenCrowdsale is TimedCrowdsale, MintedCrowdsale {
+    function ebiketokenCrowdsale
+        (
+            uint256 _openingTime,
+            uint256 _closingTime,
+            uint256 _rate,
+            address _wallet,
+            MintableToken _token
+        )
+        public
+        Crowdsale(_rate, _wallet, _token)
+        TimedCrowdsale(_openingTime, _closingTime) {
+
+        }
+}
